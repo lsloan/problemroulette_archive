df210928ca6205f117b5dcdda1d4420dba6c7760
jaredtritz <jtritz@umich.edu>
2013-09-23 19:14:30 -0400

    added some code to fake the mysqldump by creating statements directly in php

--- download_db.php
+++ download_db.php
@@ -23,12 +23,53 @@ $staff = $usrmgr->m_user->staff;
 if ($staff != 1) 
     header('Location: ' . $GLOBALS["DOMAIN"]);
 
-$cmd = "mysqldump --user='" . $GLOBALS["SQL_USER"] . "' --password='" . $GLOBALS["SQL_PASSWORD"] . "' --host='" . $GLOBALS["SQL_SERVER"] ."' ". $GLOBALS["SQL_DATABASE"] . " > " . $GLOBALS["DIR_DOWNLOADS"] . "down.sql";
+#$cmd = "mysqldump --user='" . $GLOBALS["SQL_USER"] . "' --password='" . $GLOBALS["SQL_PASSWORD"] . "' --host='" . $GLOBALS["SQL_SERVER"] ."' ". $GLOBALS["SQL_DATABASE"] . " > " . $GLOBALS["DIR_DOWNLOADS"] . "down.sql";
+#exec($cmd);
 
-exec($cmd);
+   
+global $dbmgr; 
+//get all of the tables
+$tables = array();
+$result = $dbmgr->fetch_num('SHOW TABLES');
+foreach($result as $row)
+{
+    $tables[] = $row[0];
+}
+//cycle through them
+$return = "";
+foreach($tables as $table)
+{
+    $result = $dbmgr->fetch_num('SELECT * FROM '.$table);
+    $num_fields = count($result);
+    
+    $return .= 'DROP TABLE '.$table.';';
+    $row2 = $dbmgr->fetch_num('SHOW CREATE TABLE '.$table);
+    $return .= "\n\n".$row2[0][1].";\n\n";
+    
+    foreach($result as $res)
+    {
+        $return .= 'INSERT INTO '.$table.' VALUES(';
+        foreach($res as $ii) 
+        {
+            $ii = addslashes($ii);
+            $ii = ereg_replace("\n","\\n",$ii);
 
+            if (isset($ii)) 
+            { 
+                $return .= '"'.$ii.'",' ; 
+            } 
+            else 
+            { 
+                $return .= '"",'; 
+            }
+        }
+        $return = substr($return, 0, -1);
+        $return .= ");\n";
+    }
+    $return .= "\n\n\n";
+}
 $file = $GLOBALS["DIR_DOWNLOADS"].'down.sql';
-
+file_put_contents($file, $return);
 if (file_exists($file)) {
     header('Content-Description: File Transfer');
     header('Content-Type: application/octet-stream');
--- lib/usrmgr.php
+++ lib/usrmgr.php
@@ -107,7 +107,8 @@ class UserManager{
 	function Login()
     {
         // set any default (in deveopement this will be the active user_id)		
-        $username = 'test_user8';
+        //$username = 'test_user8';
+        $username = 'jtritz';
         // check if the user just logged in through cosign
         if(isset($_SERVER['REMOTE_USER']))
             $username = $_SERVER["REMOTE_USER"];
