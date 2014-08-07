48cac3430c503e84a423aa13195053f6f4b7c6a1
jleasia <jleasia@umich.edu>
2014-08-07 14:57:59 -0400

    Simpler, cleaner way of doing the same thing.
    submit wasn't needed, so can combine the two functions into one and use this instead of getElementById

--- js/problem.js
+++ js/problem.js
@@ -2,13 +2,8 @@ $(document).ready(function()
 {
   /* problem submit, skip button hiding to avoid double clicks
      on the problem submit page, in views.php */
-  $("#submit_answer").click(function() {
-      document.getElementById('submit_answer').style.visibility='hidden' ;
-      document.ans_form.submit();
-    });
-
-  $("#skip").click(function() {
-      document.getElementById('skip').style.visibility='hidden' ;
-    });
+  $('#submit_answer, #skip').click(function() {
+    $(this).css('visibility', 'hidden');
+  });
 
 });
