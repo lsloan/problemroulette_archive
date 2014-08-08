c54c6b970f57a88b56f43da31718b5813d80fec9
Eric Echeverri <ericeche@umich.edu>
2014-08-08 16:25:20 -0400

    Removing unused code

--- cleanup/roulette.php
+++
@@ -1,3 +0,0 @@
-<?php include 'start.php'; ?>
-	<iframe src="roulette-problems.php"></iframe>
-<?php include 'end.php'; ?>
--- cleanup/statistics.php
+++
@@ -1,3 +0,0 @@
-<div style="height: 500px; width: 500px; background-color: grey;">
-<?php echo time() ?>
-</div>
