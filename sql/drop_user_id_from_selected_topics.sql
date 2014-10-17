32a64711e1b0dc75951a201aee258b04a248fffe
Jim Eng <jimeng@umich.edu>
2014-10-17 16:02:52 -0400

    PR-39 - Update migration to add selection_id to selected_topics before removing user_id column.

--- sql/drop_user_id_from_selected_topics.sql
+++ sql/drop_user_id_from_selected_topics.sql
@@ -1,2 +1,6 @@
-drop index selected_topics_idx on selected_topics;
-alter table selected_topics drop column user_id;
+alter table `selected_topics` add column `selection_id` int(11);
+add UNIQUE index `selection_topic_idx` on `selected_topics` (`selection_id`,`topic_id`);
+update `selected_topics` t1, `selections` t2, `12m_class_topic` t3  set t1.selection_id=t2.id where t1.topic_id=t3.topic_id and t2.class_id=t3.class_id and t2.user_id=t1.user_id;
+
+drop index `selected_topics_idx` on `selected_topics`;
+alter table `selected_topics` drop column `user_id`;
