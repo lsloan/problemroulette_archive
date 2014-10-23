0e16a77e588847cf00eb5e86213ec5d5e7dbf20b
Eric Echeverri <ericeche@umich.edu>
2014-10-23 12:49:12 +0000

    Bundling include files into include_all_libs.php

--- init_user_columns.php
+++ init_user_columns.php
@@ -1,17 +1,6 @@
 <?php
-// paths
-require_once("./paths.inc.php");
-// database
-require_once( $GLOBALS["DIR_LIB"]."dbmgr.php" );
-$GLOBALS["dbmgr"] = new CDbMgr();
-// user manager
-require_once( $DIR_LIB."usrmgr.php" );
-$GLOBALS["usrmgr"] = new UserManager();
-// utilities
-require_once($GLOBALS["DIR_LIB"]."utilities.php");
-$args = GrabAllArgs();
-// application objects
-require_once($GLOBALS["DIR_LIB"]."models.php");
+
+require_once("./include_all_libs.php");
 
 global $dbmgr;
 global $usrmgr;
--- stats.php
+++ stats.php
@@ -1,18 +1,6 @@
 <?php
-// paths
-require_once("./paths.inc.php");
-// database
-require_once( $GLOBALS["DIR_LIB"]."dbmgr.php" );
-$GLOBALS["dbmgr"] = new CDbMgr();
-// user manager
-require_once( $DIR_LIB."usrmgr.php" );
-$GLOBALS["usrmgr"] = new UserManager();
-// utilities
-require_once($GLOBALS["DIR_LIB"]."utilities.php");
-$args = GrabAllArgs();
-// application objects
-require_once($GLOBALS["DIR_LIB"]."models.php");
-require_once($GLOBALS["DIR_LIB"]."views.php");
+
+require_once("./include_all_libs.php");
 
 session_start();
 $_SESSION['sesstest'] = 1;
