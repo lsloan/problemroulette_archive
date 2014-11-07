0b767cb697ec990faf8345b869a77179f802e4a9
Eric Echeverri <ericeche@umich.edu>
2014-11-07 15:41:07 +0000

    Styling the Error page 500

--- lib/errors.php
+++ lib/errors.php
@@ -20,19 +20,24 @@ function shutdownHandle() {
     ) {
        ob_clean();
        header( 'HTTP/1.1 500 Internal Server Error' );
-       echo '<div class="error-page">';
-       echo '<h1>Internal Server Error</h1>';
-       echo "<img class='logo' src='img/PR.jpg' width='200px' alt='Problem Roulette'/>";
-       echo '<p>';
-       echo 'Please contact <a href="mailto:physics.sso@umich.edu">physics.sso@umich.edu</a> with any problems.';
-       echo '</p>';
-       // whatever type of output you want including mail(), public message, etc.
-       // in this case debug info
-       print_r($e);
-       echo '</div>';
-       exit(1);
-   }
-
-}
-
 ?>
+    <!DOCTYPE HTML>
+    <html>
+    <head>
+    <title>Internal Server Error</title>
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+    </head>
+    <body>
+        <div class="error-page">
+            <h1>Internal Server Error</h1>
+            <img class='logo' src='img/PR.jpg' width='200px' alt='Problem Roulette'/>
+            <p>
+                Please contact <a href="mailto:physics.sso@umich.edu">physics.sso@umich.edu</a> with any problems.
+            </p>
+            <pre><?php echo htmlentities(print_r($e,true));?></pre>
+        </div>
+    </body>
+<?php
+    exit(1);
+   }
+}
