99bd7019ccb629e3ae85c092bcf8e80b596c16a8
Jim Eng <jimeng@umich.edu>
2014-11-26 08:45:55 -0500

    PR-43 - Remove initialization of 'user.admin' values from migration.

--- migrations/add_admin_to_user.php
+++ migrations/add_admin_to_user.php
@@ -7,18 +7,13 @@ $this->add_admin =<<<SQL
 alter table user add column `admin` int(1) NOT NULL default 0
 SQL;
 
-$this->add_values =<<<SQL
-update user set admin=1 where username='jleasia' or username='botimer' or username='ericeche' or username='jimeng'
-SQL;
-
 $this->verify =<<<SQL
-SELECT count(*) FROM user where admin=1
+show columns from user
 SQL;
     }
 
     function migrate() {
         $this->db->exec_query($this->add_admin);
-        $this->db->exec_query($this->add_values);
         $res = $this->db->fetch_assoc($this->verify);
         ob_start();
         print_r($res);
