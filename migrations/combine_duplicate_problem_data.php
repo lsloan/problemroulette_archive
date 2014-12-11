32632795e228a9643140c200d512b1dae5ac554e
Jim Eng <jimeng@umich.edu>
2014-12-11 09:07:51 -0500

    PR-58 Remove comment from migration file

--- migrations/combine_duplicate_problem_data.php
+++ migrations/combine_duplicate_problem_data.php
@@ -1,25 +1,4 @@
 <?php
-// This is an example migration, showing the two portions of the base class
-// that will typically be overridden: init() and migrate(). It also shows
-// the capture and logging of some output at the INFO level.
-//
-// Be sure to register the class name and filename in upgrade.php when
-// implementing real migrations; otherwise, they will not be run.
-//
-// The Migration class and the usual utilities will already be loaded, so
-// boilerplate require statements are not required. The database manager will
-// be available as $this->db.
-//
-// The default init() and migrate() implementations do nothing, so there is no
-// need to call the super class.
-//
-// The init() method is called during the constructor, and should be used to set
-// up any instance variables needed.  A common pattern here is to set up
-// parameterized queries as heredocs (which do not work as static members).
-//
-// The migrate() method represents the bulk of the migration operation. Any work
-// to be done should be called during this method. Any output should be done
-// through the logging methods (e.g., info(), error()).
 
 class CombineDuplicateProblemData extends Migration {
 
