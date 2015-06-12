cc7aef4c68d6c3133108b52b0829e8071f634ab0
Noah Botimer <botimer@umich.edu>
2015-06-12 13:57:47 -0400

    PR-115 - Use dirname instead of __DIR__ for PHP 5.2

--- api/v1/courses.php
+++ api/v1/courses.php
@@ -1,5 +1,5 @@
 <?php
-require_once(__DIR__.'/../rest.php');
+require_once(dirname(__FILE__).'/../rest.php');
 
 class CoursesResource extends Resource {
     function get($path, $params) {
--- setup.php
+++ setup.php
@@ -1,6 +1,6 @@
 <?php
 // paths
-require_once(__DIR__."/paths.inc.php");
+require_once(dirname(__FILE__)."/paths.inc.php");
 // error
 require_once($GLOBALS["DIR_LIB"]."errors.php");
 
