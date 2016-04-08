864877229da6b9c8487b8e14c860e35cd5e48fa1
pushyami <pushyami@umich.edu>
2016-04-08 16:05:28 -0400

    TLCALIPER-36 Using Viadutoo with caliper

--- lib/caliper_config.php
+++ lib/caliper_config.php
@@ -4,22 +4,37 @@ require_once 'vendor/autoload.php';
 require_once 'Caliper/Options.php';
 
 class CaliperConfig extends Options{
-    private $sensor_id;
-    private $caliper_http_id;
-    private $caliper_client_id;
+    private $sensor_id=false;
+    private $caliper_http_id=false;
+    private $caliper_client_id=false;
+    private $caliper_proxy_url=false;
+    private $caliper_proxy_enabled=false;
+    private $ca_certs_path=false;
 
 
     public function getSensorId()
     {
         return $this->sensor_id;
     }
+    public function setSensorId($sensor_id)
+    {
+        if(!is_string($sensor_id)){
+            $sensor_id=strval($sensor_id);
+        }
+        $this->sensor_id = $sensor_id;
+        return $this;
+    }
 
     public function getCaliperHttpId()
     {
         return $this->caliper_http_id;
     }
+
     public function setCaliperHttpId($caliper_http_id)
     {
+        if(!is_string($caliper_http_id)){
+            $caliper_http_id=strval($caliper_http_id);
+        }
         $this->caliper_http_id = $caliper_http_id;
         return $this;
     }
@@ -31,13 +46,54 @@ class CaliperConfig extends Options{
 
     public function setCaliperClientId($caliper_client_id)
     {
+        if(!is_string($caliper_client_id)){
+            $caliper_client_id=strval($caliper_client_id);
+        }
         $this->caliper_client_id = $caliper_client_id;
         return $this;
     }
 
-    public function setSensorId($sensor_id)
+    public function getCaliperProxyUrl()
     {
-        $this->sensor_id = $sensor_id;
+        return $this->caliper_proxy_url;
+    }
+
+    public function setCaliperProxyUrl($caliper_proxy_url)
+    {
+        if(!is_string($caliper_proxy_url)){
+            $caliper_proxy_url=strval($caliper_proxy_url);
+        }
+        $this->caliper_proxy_url = $caliper_proxy_url;
         return $this;
     }
+
+    public function getCaliperProxyEnabled()
+    {
+        return $this->caliper_proxy_enabled;
+    }
+
+    public function setCaliperProxyEnabled($caliper_proxy_enabled)
+    {
+        if(!is_bool($caliper_proxy_enabled)){
+            $caliper_proxy_enabled=boolval($caliper_proxy_enabled);
+        }
+        $this->caliper_proxy_enabled = $caliper_proxy_enabled;
+        return $this;
+    }
+
+    public function getCaCertsPath()
+    {
+        return $this->ca_certs_path;
+    }
+
+    public function setCaCertsPath($ca_certs_path)
+    {
+        if(!is_string($ca_certs_path)){
+            $ca_certs_path=strval($ca_certs_path);
+        }
+        $this->ca_certs_path = $ca_certs_path;
+        return $this;
+    }
+
+
 }
